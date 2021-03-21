688ee386887b9f7506f81469f197ad265462d7aa
Amanda Rodriguez <amandardz15@gmail.com>
2021-03-20 20:54:34 -0500

    modifying files

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var currentDate = new Date().toLocaleDateString('en-US')
 
 var printCityList = function(name) {
     var cityListItem = document.createElement('li')
-    cityListItem.classList.add('list-unstyled', 'list-group-item', 'list-group-item-action', 'mt-1')
+    cityListItem.classList.add('list-unstyled', 'list-group-item', 'list-group-item-action', 'h6', 'mt-1')
     cityListItem.textContent = name;
     cityListEl.appendChild(cityListItem)
 
@@ -33,7 +33,7 @@ var getCurrentWeather = function (cityName) {
 
             var currentWeatherDiv = document.createElement('div')
             currentWeatherEl.appendChild(currentWeatherDiv)
-            currentWeatherDiv.innerHTML = '<h2>' + data.name + ' ' + currentDate + ' ' + '<img src="' + iconUrl + '" alt="Weather Icons">' + '</h2>' + '<p>' + "Temp: " + Math.round(data.main.temp) + " ℉" + '</p>' + '<p>' + "Humid: " + data.main.humidity + "%" + '</p>' + '<p>' + "Wind Speed: " + data.wind.speed + " MPH" + '</p>'
+            currentWeatherDiv.innerHTML = '<h2>' + data.name + ' ' + currentDate + ' ' + '<img src="' + iconUrl + '" alt="Weather Icons">' + '</h2>' + '<h6>' + '<p>' + "Temp: " + Math.round(data.main.temp) + " ℉" + '</p>' + '<p>' + "Humid: " + data.main.humidity + "%" + '</p>' + '<p>' + "Wind Speed: " + data.wind.speed + " MPH" + '</p>' + '</h6>'
 
             var uvUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=current,minutely,hourly,alerts&units=imperial&appid=f05e59dca587993db2e06e2c3a372a11'
 
@@ -45,7 +45,7 @@ var getCurrentWeather = function (cityName) {
                     console.log(data)
                     var uv1El = document.createElement('div')
                     currentWeatherDiv.appendChild(uv1El)
-                    uv1El.textContent = "UV Index: " + data.daily[0].uvi
+                    uv1El.innerHTML = '<h6>' + "UV Index: " + data.daily[0].uvi + '</h6>'
         
                     if(data.daily[0].uvi === 0 || data.daily[0].uvi < 3){
                         uv1El.setAttribute("style", "color: green;")
@@ -55,13 +55,17 @@ var getCurrentWeather = function (cityName) {
                         uv1El.setAttribute("style", "color: red;")
                     }
 
+                    var weeklyLabelEl = document.querySelector('#weeklyLabel')
+                    weeklyLabelEl.innerHTML = '<h3>' + '5-Day Forecast:' + '</h3>'
+
                     for(var i = 1; i < 6; i++){
                         var dailyDate = new Date(data.daily[i].dt * 1000).toLocaleDateString('en-US')
                         var dailyWeatherIcon = data.daily[i].weather[0].icon
                         var iconUrl = 'https://www.openweathermap.org/img/wn/' + dailyWeatherIcon + '.png'
                         var weeklyDiv = document.createElement('div')
+                        weeklyDiv.classList.add('weekly', 'rounded', 'card', 'p-3', 'mr-3', 'bg-gradient')
                         weeklyEl.appendChild(weeklyDiv)
-                        weeklyDiv.innerHTML = '<p>' + dailyDate + '</p>' + '<p>' +  '<img src="' + iconUrl + '" alt="Weather Icons">' + '</p>' + '<p>' + "Temp: " + Math.round(data.daily[i].temp.max) + " ℉" + '</p>' + '<p>' + "Humid: " + data.daily[i].humidity + "%" + '</p>'
+                        weeklyDiv.innerHTML = '<h6>' + '<p>' + dailyDate + '</p>' + '<p>' +  '<img src="' + iconUrl + '" alt="Weather Icons">' + '</p>' + '<p>' + "Temp: " + Math.round(data.daily[i].temp.max) + " ℉" + '</p>' + '<p>' + "Humid: " + data.daily[i].humidity + "%" + '</p>' + '</h6>'
                     }
                 })
         })
